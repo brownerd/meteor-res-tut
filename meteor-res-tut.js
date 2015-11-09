1487e4a916db1c35d3443840631c9159eb994e3b
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2015-11-09 13:11:30 -0600

    lesson 6 done

--- meteor-res-tut.js
+++ meteor-res-tut.js
@@ -12,7 +12,33 @@ if (Meteor.isClient) {
       return Resolutions.find()
     }
   })
+
+  Template.body.events({
+    'submit .new-resolution': function(event) {
+      var title = event.target.title.value;
+
+      Resolutions.insert({
+        title : title,
+        createdAt: new Date()
+      })
+
+      event.target.title.value = "";
+
+      return false;
+    }
+  })
+
+
+  Template.resolution.events({
+    'click .toggle-checked': function() {
+      Resolutions.update(this._id, {$set: {checked: !this.checked}})
+    },
+    'click .delete': function() {
+      Resolutions.remove(this._id)
+    }
+  })
 }
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {

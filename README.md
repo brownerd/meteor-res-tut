ba30bf9acb809ca5095b379eeb962994cad65966
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2015-11-09 12:42:08 -0600

    lesson 4 done

---
+++ README.md
@@ -0,0 +1,5 @@
+# Meteor Resolutions Tutorial by LevelUp tuts
+
+Just learning a little Meteor here.
+
+[Meteor For Everyone](http://leveluptuts.com/tutorials/meteor-for-everyone)
--- meteor-res-tut.js
+++ meteor-res-tut.js
@@ -1,10 +1,16 @@
+// Creating a new colleciton in MongoDB
+Resolutions = new Mongo.Collection('resolutions')
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    resolutions: [
-      {title: "Hello Resolution #1"},
-      {title: "Bye Resolution #2"},
-      {title: "What up!"}
-    ]
+    // resolutions: [
+    //   {title: "Hello Resolution #1"},
+    //   {title: "Bye Resolution #2"},
+    //   {title: "What up!"}
+    // ]
+    resolutions: function() {
+      return Resolutions.find()
+    }
   })
 }
 
